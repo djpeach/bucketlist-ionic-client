05f26e1a68cd0ba46bf636fdce1feb47758b3026
Daniel Peach <dpeaches96@gmail.com>
2019-12-02 18:46:57 -0500

    deployable

--- src/conf/routes.js
+++ src/conf/routes.js
@@ -20,3 +20,14 @@ export default {
   },
   addFriend: '/friends/new',
 }
+
+export const routeWithParams = (route, ...params) => {
+  const re = /(?:\/):\w+/g
+  let i = 0
+  let match
+  while ((match = re.exec(route)) != null) {
+    route = route.replace(match[0], `/${params[i]}`)
+    i++
+  }
+  return route
+}
