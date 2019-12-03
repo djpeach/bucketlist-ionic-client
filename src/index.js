065565f999a303a11883be7845b2ab53a4432dcb
Daniel Peach <dpeaches96@gmail.com>
2019-12-03 03:21:08 -0500

    beginning testing, switching to heroku integration for ci

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import keys from './conf/keys'
 import App from './App'
 
 const httpLink = new HttpLink({
-  uri: 'http://server.bucketlist.group/graphql',
+  uri: 'https://bucketlistgroup.herokuapp.com/graphql',
 })
 
 const authMiddleware = new ApolloLink((operation, forward) => {
