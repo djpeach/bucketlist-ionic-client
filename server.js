29afcfd67aee1fe92d1e5463e110b930eafaa172
Daniel Peach <dpeaches96@gmail.com>
2019-12-03 12:01:03 -0500

    loggin on server load

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(80, () => {
+  console.log('Running on port 80')
+});
