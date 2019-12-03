f00236f61a22c07d6e489cdb5a72ba75396ad8b2
Daniel Peach <dpeaches96@gmail.com>
2019-12-02 20:04:57 -0500

    renaming

--- src/components/user/index.js
+++ src/components/user/index.js
@@ -1,6 +1,6 @@
-export { default as Buckets, ListOfBuckets } from './Buckets';
+export { default as Buckets, ListOfBuckets } from './BucketsListView';
 export { default as Dashboard } from './Dashboard';
-export { default as Bucket } from './Bucket';
+export { default as Bucket } from './BucketDetailView';
 export { default as BucketsPreview } from './BucketsPreview';
 export { default as NewDrop } from './NewDrop';
 export { default as More } from './ProfileSettings';
