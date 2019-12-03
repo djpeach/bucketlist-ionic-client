be84715aec1feb439d3070a72a12e99d8707bf8c
Nathalie Kroeker <nathaliekroeker@gmail.com>
2019-12-02 22:44:33 -0500

    Make modal pretty and fix myself

--- src/components/user/NewDrop.js
+++ src/components/user/NewDrop.js
@@ -13,6 +13,7 @@ import { useQuery, useMutation } from '@apollo/react-hooks'
 import gql from '../../graphql'
 import firebase from 'firebase'
 import routes from '../../conf/routes';
+import { ListOfBuckets } from '.';
 
 const friendSearchStyles = {
   menu: (provided, state) => ({
@@ -27,7 +28,9 @@ const friendSearchStyles = {
 
 function NewDrop(props) {
   const self = { id: firebase.auth().currentUser.uid, firstName: 'Myself', lastName: '' };
-  const [friendObj, setFriendObj] = useState(self)
+  const selfOption = { value: self.id, label: 'Myself' };
+  const [friendObj, setFriendObj] = useState(selfOption)
+  const [selectedBucket, changeSelectedBucket] = useState('')
   const [message, setMessage] = useState('')
   const { loading, error, data } = useQuery(gql.getAllFriends, { variables: { userId: firebase.auth().currentUser.uid } })
   const [createItem] = useMutation(gql.createItem, {
@@ -44,21 +47,28 @@ function NewDrop(props) {
 
   const handleSubmit = () => {
     if (friendObj) {
-      createItem({
-        variables: {
-          senderId: firebase.auth().currentUser.uid,
-          recipientId: friendObj.id,
-          message: message
-        }
-      })
+      const itemObj = {
+        senderId: firebase.auth().currentUser.uid,
+        recipientId: friendObj.id,
+        message: message
+      };
+      if (selectedBucket) {
+        createItem({
+          variables: { ...itemObj, listId: selectedBucket }
+        })
+      } else {
+        createItem({
+          variables: itemObj
+        })
+      }
     }
   }
 
   const friends = (loading || error || data.getAllFriends.length <= 0 ? [{ id: 0, firstName: 'No', lastName: ' Friends :(' }] : data.getAllFriends)
 
-  if (!friends.some(f => f.firstName === 'Myself')) {
-    friends.unshift(self)
-  }
+  // if (!friends.some(f => f.firstName === 'Myself')) {
+  //   friends.unshift(self)
+  // }
   friends.map((obj) => {
     obj.value = obj.id
     obj.label = `${obj.firstName} ${obj.lastName}`
@@ -69,27 +79,33 @@ function NewDrop(props) {
     <IonPage className="bl-page">
       <IonCard className="bl-card-padding">
         <h1 style={{ paddingBottom: '20px' }}>New Drop</h1>
+        <form onSubmit={handleSubmit}>
+          <Select
+            value={friendObj}
+            placeholder='Search Friends'
+            styles={friendSearchStyles}
+            onChange={handleFriendSearchChange}
+            noOptionsMessage={() => 'Friend not found'}
+            isClearable
+            isSearchable
+            name="friends"
+            options={[selfOption, ...friends]}
+          />
 
-        <Select
-          value={friendObj}
-          placeholder='Search Friends'
-          styles={friendSearchStyles}
-          onChange={handleFriendSearchChange}
-          noOptionsMessage={() => 'Friend not found'}
-          isClearable
-          isSearchable
-          name="friends"
-          options={friends}
-        />
-
-        <IonItem style={{ marginTop: '20px' }}>
-          <IonLabel position="floating"></IonLabel>
-          <IonInput placeholder={"Description"} onIonChange={(e) => setMessage(e.target.value)} value={message} />
-        </IonItem>
-        <IonButton color="success" strong type="button"
-          className="ion-float-right ion-margin-end ion-margin-bottom bl-new-list-btn" style={{ marginTop: '20px' }} onClick={handleSubmit}>
-          Send Drop
-        </IonButton>
+          <IonItem style={{ marginTop: '20px' }}>
+            <IonLabel position="floating">Description</IonLabel>
+            <IonInput placeholder="e.g. go on an adventure" required onIonChange={(e) => setMessage(e.target.value)} value={message} />
+          </IonItem>
+          {friendObj.firstName === 'Myself' && (
+            <IonItem>
+              <ListOfBuckets onSelected={changeSelectedBucket} />
+            </IonItem>
+          )}
+          <IonButton color="success" strong type="button" type="submit"
+            className="ion-float-right ion-margin-end ion-margin-bottom bl-new-list-btn" style={{ marginTop: '20px' }}>
+            Send Drop
+          </IonButton>
+        </form>
       </IonCard>
     </IonPage>
   )
