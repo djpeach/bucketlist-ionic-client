07f28921b290f1715a88b343b19257e09b5d84f3
Trenton Spice <tspice23@att.net>
2019-12-03 20:28:46 -0500

    front end test for if a user is logged in or not

--- src/components/user/Dashboard.test.js
+++ src/components/user/Dashboard.test.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import firebase from 'firebase'
 import keys from '../../conf/keys'
+import routes from '../../conf/routes'
+
 
 firebase.initializeApp(keys.firebaseConfig)
 
 import Dashboard from './Dashboard'
 
+let wrapper
+let state
+
 describe('Dashboard unit tests', () => {
+  beforeEach(() => {
+    wrapper = shallow(<Dashboard />)
+  })
   it('should render', () => {
-    const component = shallow(<Dashboard />);
-    expect(component).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('redirects to login if not logged in', () => {
+    expect(wrapper.find('Redirect').prop('to')).toBe("/auth/login")
   })
 })
